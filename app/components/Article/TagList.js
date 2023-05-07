95e3667447c2b139f8ff5181e15b8c6bd3b9744b
HoangHuynh145 <95960229+HoangHuynh145@users.noreply.github.com>
2023-05-07 16:51:13 +0700

    fix error when change tag

--- app/components/Article/TagList.js
+++ app/components/Article/TagList.js
@@ -1,15 +1,23 @@
+import { usePageContext } from '@/app/context/PageContext'
 import { useTagContext } from '@/app/context/TagContext'
 import React from 'react'
 
 const TagList = ({ tags }) => {
     const { setSelectedTag } = useTagContext()
+    const { setCurrentPage } = usePageContext()
+
+
+    const handleChangeTag = (tag) => {
+        setSelectedTag(tag)
+        setCurrentPage(1)
+    }
 
     return (
         tags.map(tag => (
             <li
                 key={tag}
                 className='bg-gray-500/80 hover:bg-gray-500 px-1.5 py-0.5 cursor-pointer rounded-full text-white text-xs'
-                onClick={() => setSelectedTag(tag)}
+                onClick={() => handleChangeTag(tag)}
             >
                 {tag}
             </li>
